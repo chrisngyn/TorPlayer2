9cb4881a90a7e8bf67a3e65884cdacf6bfd4b860
ChienNM3 <chiennm3@vng.com.vn>
2024-01-24 09:59:49 +0700

    Integrate system tray

--- go.mod
+++ go.mod
@@ -4,15 +4,25 @@ go 1.21
 
 require (
 	github.com/a-h/templ v0.2.513
+	github.com/adrg/xdg v0.4.0
 	github.com/anacrolix/torrent v1.53.3
 	github.com/asticode/go-astisub v0.26.2
 	github.com/gabriel-vasile/mimetype v1.4.3
 )
 
 require (
-	github.com/adrg/xdg v0.4.0 // indirect
 	github.com/asticode/go-astikit v0.20.0 // indirect
 	github.com/asticode/go-astits v1.8.0 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/getlantern/systray v1.2.2 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
 
@@ -79,7 +89,7 @@ require (
 	go.opentelemetry.io/otel v1.8.0 // indirect
 	go.opentelemetry.io/otel/trace v1.8.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"embed"
-	"fmt"
+	"errors"
 	"log"
 	"log/slog"
 	"net/http"
 
+	"github.com/getlantern/systray"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/pkg/browser"
 
 	"TorPlayer2/handler"
 	"TorPlayer2/request"
@@ -19,14 +21,24 @@ import (
 //go:embed static
 var fs embed.FS
 
+//go:embed static/appicon.ico
+var appIcon []byte
+
+type closeFn struct {
+	name string
+	fn   func() error
+}
+
 func main() {
+	var closeFns []closeFn
+
 	settingStorage := setting.NewStorage()
-	defer executeCleanupFunc("setting storage", settingStorage.CleanUp)
+	closeFns = append(closeFns, closeFn{"setting storage", settingStorage.CleanUp})
 
 	settings := settingStorage.GetSetting()
 
 	m := torrent.NewManager(settings.DataDir)
-	defer executeCleanupFunc("torrent manager", m.Close)
+	closeFns = append(closeFns, closeFn{"torrent manager", m.Close})
 
 	r := chi.NewRouter()
 
@@ -40,15 +52,54 @@ func main() {
 	httpHandler := handler.New(m, settingStorage)
 	httpHandler.Register(r)
 
-	slog.Info("Starting server on port 8080\n")
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
 	}
+	closeFns = append(closeFns, closeFn{"http server", httpServer.Close})
+	go func() {
+		slog.Info("Starting server on port 8080\n")
+		if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	onExit := func() {
+		for _, closeFn := range closeFns {
+			if err := closeFn.fn(); err != nil {
+				slog.With("error", err, "name", closeFn.name).Error("Failed to close")
+			}
+		}
+	}
+
+	systray.Run(setupOnReady("http://localhost:8080"), onExit)
 
 }
 
-func executeCleanupFunc(entityName string, cleanupFunc func() error) {
-	if err := cleanupFunc(); err != nil {
-		slog.With("error", err).Error(fmt.Sprintf("Failed to clean up %s", entityName))
+func setupOnReady(serverAddr string) func() {
+	return func() {
+		systray.SetIcon(appIcon)
+		systray.SetTitle("TorPlayer2")
+		systray.SetTooltip("TorPlayer2")
+		mOpenBrowser := systray.AddMenuItem("Open Torplay web", "Open Torplay in browser")
+		systray.AddSeparator()
+		mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+
+		go func() {
+			for {
+				select {
+				case <-mOpenBrowser.ClickedCh:
+					err := browser.OpenURL(serverAddr)
+					if err != nil {
+						slog.With("error", err).Error("Failed to open browser")
+					}
+				case <-mQuit.ClickedCh:
+					systray.Quit()
+					return
+				}
+			}
+		}()
+
+		_ = browser.OpenURL(serverAddr)
 	}
 }
